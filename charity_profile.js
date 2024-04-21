57afc765f8914225d1c7b9d93556f68ff26989e1
smoothmd <148928985+smoothmd@users.noreply.github.com>
2024-04-21 14:45:32 -0400

    Update charity_profile.js

--- charity_profile.js
+++ charity_profile.js
@@ -40,7 +40,7 @@ function displayAdditionalInfo() {
 function fetchDataFromGoogleMapsAPI() {
     // Construct the URL for the API request
     const apiKey = 'AIzaSyCzvMCLb5M-r6rR2AZkrUO6Tt-5Qj_NNDg'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCzvMCLb5M-r6rR2AZkrUO6Tt-5Qj_NNDg`;
+    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=AIzaSyCzvMCLb5M-r6rR2AZkrUO6Tt-5Qj_NNDg`.getElementById('map-map');
     // Make the HTTP GET request using fetch
     fetch(apiUrl)
         .then(response => response.json())
